4515cd2ffe6e49daa9945b807d4933a1ee361815
2clubs <bennettdan85@gmail.com>
2023-05-16 10:57:22 -0400

    frontend first commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <nav className='nav'>
+      <Link to="/">
+        <div>People App </div>
+      </Link>
+    </nav>
+  )
+}
+
+export default Header
---
+++ src/components/Main.js
@@ -0,0 +1,71 @@
+import { useEffect, useState } from 'react'
+import Index from "../pages/Index"
+import Show from "../pages/Show"
+import { Route } from 'react-router-dom'
+import { Routes } from 'react-router-dom'
+
+const Main = (props) => {
+  const [ people, setPeople ] = useState(null)
+
+  // const URL = "http://localhost:4000/people/";
+  const URL = "https://person-app-backend-zqs8.onrender.com/people";
+
+  const getPeople = async () => {
+    const response = await fetch(URL);
+    const data = await response.json();
+    setPeople(data);
+  };
+
+  const createPeople = async (person) => {
+    await fetch(URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "Application/json"
+      },
+      body: JSON.stringify(person)
+    })
+    getPeople()
+  }
+
+  const updatePeople = async (person, id) => {
+    await fetch(URL + id, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "Application/json"
+      },
+      body: JSON.stringify(person)
+    })
+    getPeople()
+  }
+
+  const deletePeople = async id => {
+    // make delete request to create people
+    await fetch(URL + id, {
+      method: "DELETE",
+    })
+    // update list of people
+    getPeople();
+  }
+
+  useEffect( () => getPeople, [])
+
+  return (
+    <main>
+    <Routes>
+      <Route exact path="/" element={<Index people={people} createPeople={createPeople} />} />  
+      <Route
+       path="/people/:id"
+        element = {
+        <Show
+          people={people}
+          updatePeople={updatePeople}
+          deletePeople={deletePeople} 
+        />}
+      />
+    
+    </Routes>
+    </main>
+  )
+}
+
+export default Main
